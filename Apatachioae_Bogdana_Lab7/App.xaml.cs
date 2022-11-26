a25c8e14fee9a844ea3cb4918855a67a1af61195
bapat <bapat@LAPTOP-RU0UKB1O>
2022-11-26 19:19:14 +0200

    Add project files.

---
+++ Apatachioae_Bogdana_Lab7/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Apatachioae_Bogdana_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Apatachioae_Bogdana_Lab7/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Apatachioae_Bogdana_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
