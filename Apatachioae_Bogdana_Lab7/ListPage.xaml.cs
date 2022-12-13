9ff3d8581e0735d9d050878ccf3137bbc7ccbf90
Bogdana16 <bapatachioae@gmail.com>
2022-12-13 15:48:25 +0200

    Lab9

--- Apatachioae_Bogdana_Lab7/ListPage.xaml.cs
+++ Apatachioae_Bogdana_Lab7/ListPage.xaml.cs
@@ -21,4 +21,33 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+
+    async void DeleteButtonClicked(object sender, EventArgs e)
+    {
+       
+        Product p=new Product();
+        if (listView.SelectedItem != null)
+        {
+            p = listView.SelectedItem as Product;
+            await App.Database.DeleteProductAsync(p);
+           
+        }
+    }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
 }
---
+++ Apatachioae_Bogdana_Lab7/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Apatachioae_Bogdana_Lab7.Models
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        [OneToMany]
+        public List<ListProduct> ListProducts { get; set; }
+    }
+}
